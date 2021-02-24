dc7d8ba4d36c0bcfa88150f7fef8f0a2e61ac4fb
Egor-Slutskiy <es79645736668@gmail.com>
2021-02-24 16:53:04 +0300

    Reworked code
    Now our program can calculate two numeral in console

--- first_git_code.py
+++ first_git_code.py
@@ -1,1 +1,11 @@
-print("Hello")
+input_msg = input("Input two numeral: ")
+x, y = input_msg.split(" ")
+x = int(x)
+y = int(y)
+
+
+def some_calculations(x, y):
+    return x + y
+
+
+print(some_calculations(x, y))
